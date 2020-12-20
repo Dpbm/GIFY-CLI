77affa20c1846433afed70a49254f5689b2d7d12
DPBM <dpbm136@gmail.com>
2020-12-20 20:48:41 -0300

    added dialog to select directory to save file

--- src/main.py
+++ src/main.py
@@ -13,106 +13,126 @@ from yaspin import yaspin
 from yaspin.spinners import Spinners
 import emoji 
 import sys
+from tkinter import *
+from tkinter import filedialog 
+import asyncio
 
-PREFIX = 'https://www.youtube.com'
+async def browse_files(): 
+    root = Tk()
+    root.option_add('*foreground', 'black')  
+    root.option_add('*activeForeground', 'black')
+    root.withdraw()
+    dir_name = filedialog.askdirectory() 
+    return dir_name
+       
+       
+async def main():
+    PREFIX = 'https://www.youtube.com'
 
-table = PrettyTable(['title', 'visualizations', 'date', 'likes in video', 'deslikes in video', 'amount comments in video'])
+    table = PrettyTable(['title', 'visualizations', 'date', 'likes in video', 'deslikes in video', 'amount comments in video', 'links'])
 
-option = Options()
-option.add_argument ('--headless')  
+    option = Options()
+    option.add_argument ('--headless')  
 
-if len(sys.argv) - 1 > 2 or len(sys.argv) - 1 < 2:
-    print("You need to pass only two arguments")
-    exit(1)
+    if len(sys.argv) - 1 > 2 or len(sys.argv) - 1 < 2:
+        print("You need to pass only two arguments")
+        exit(1)
 
-url = sys.argv[1]
-file_name = sys.argv[2]
+    url = sys.argv[1]
+    out_file = sys.argv[2]
+    directory = await browse_files()
 
-browser = webdriver.Chrome(ChromeDriverManager().install(), options=option)
-browser.get(url)
-soup = BeautifulSoup(browser.page_source, 'html.parser')
+    print(emoji.emojize(f':open_file_folder:   Selected Directory: {directory}'))
 
-links = []
-
-with yaspin(Spinners.moon, text="Getting Links"):
-    for link in soup.find_all('a'):
-        if 'watch' in str(link.get('href')) and '&index=' in str(link.get('href')):
-            links.append(PREFIX + link.get('href'))
-
-print(emoji.emojize("\n\n:check_mark: Links"))
-
-
-links = remove_duplicates(links)
-
-
-titles = []
-visualizations = []
-dates = []
-likes = []
-deslikes = []
-
-
-for link in links:
-    browser.get(link)
-    page_has_loaded(browser)
-    sleep(5)
-
+    browser = webdriver.Chrome(ChromeDriverManager().install(), options=option)
+    browser.get(url)
     soup = BeautifulSoup(browser.page_source, 'html.parser')
-    title = soup.title.string.split()[:-2]
-    title = convert(title)
-    titles.append(title)
-    header = title.center(100, '-')
-    print(header)
-    print(emoji.emojize(":check_mark: Title"))
+    links = []
 
-    getVisualization = browser.find_element_by_xpath("//div[@id='info'][@class='style-scope ytd-video-primary-info-renderer']//div[@id='info-text'][@class='style-scope ytd-video-primary-info-renderer']//div[@id='count'][@class='style-scope ytd-video-primary-info-renderer']//yt-view-count-renderer[@class='style-scope ytd-video-primary-info-renderer']//span[@class='view-count style-scope yt-view-count-renderer']")
-    visualization = getVisualization.get_attribute('innerText').split()[0]
-    visualizations.append(visualization) 
-    print(emoji.emojize(":check_mark: Visualizations"))
+    with yaspin(Spinners.moon, text="Getting Links"):
+        for link in soup.find_all('a'):
+            if 'watch' in str(link.get('href')) and '&index=' in str(link.get('href')):
+                links.append(PREFIX + link.get('href'))
+
+    print(emoji.emojize("\n\n:check_mark: Links"))
 
 
-    getDate = browser.find_element_by_xpath("//*[@id='date']/yt-formatted-string")
-    date = getDate.get_attribute('innerText')
-    dates.append(date)
-    print(emoji.emojize(":check_mark: Date"))
+    links = remove_duplicates(links)
+
+
+    titles = []
+    visualizations = []
+    dates = []
+    likes = []
+    deslikes = []
+
+
+    for link in links:
+        browser.get(link)
+        page_has_loaded(browser)
+        sleep(5)
+
+        soup = BeautifulSoup(browser.page_source, 'html.parser')
+        title = soup.title.string.split()[:-2]
+        title = convert(title)
+        titles.append(title)
+        header = title.center(100, '-')
+        print(header)
+        print(emoji.emojize(":check_mark: Title"))
+
+        getVisualization = browser.find_element_by_xpath("//div[@id='info'][@class='style-scope ytd-video-primary-info-renderer']//div[@id='info-text'][@class='style-scope ytd-video-primary-info-renderer']//div[@id='count'][@class='style-scope ytd-video-primary-info-renderer']//yt-view-count-renderer[@class='style-scope ytd-video-primary-info-renderer']//span[@class='view-count style-scope yt-view-count-renderer']")
+        visualization = getVisualization.get_attribute('innerText').split()[0]
+        visualizations.append(visualization) 
+        print(emoji.emojize(":check_mark: Visualizations"))
+
+
+        getDate = browser.find_element_by_xpath("//*[@id='date']/yt-formatted-string")
+        date = getDate.get_attribute('innerText')
+        dates.append(date)
+        print(emoji.emojize(":check_mark: Date"))
 
 
 
-    get_likes_and_dislikes = browser.find_element_by_xpath("//div[@id='info']//div[@id='info-contents']//ytd-video-primary-info-renderer//div[@id='container']//div[@id='info']//div[@id='menu-container']//ytd-sentiment-bar-renderer//paper-tooltip//div[@id='tooltip']")
-    likes_and_deslikes_in_video = get_likes_and_dislikes.get_attribute('innerText').split()
-    likes_in_video = likes_and_deslikes_in_video[0]
-    deslikes_in_video = likes_and_deslikes_in_video[-1]
-    likes.append(likes_in_video)
-    print(emoji.emojize(":check_mark: Likes"))
-    deslikes.append(deslikes_in_video)
-    print(emoji.emojize(":check_mark: Deslikes"))
+        get_likes_and_dislikes = browser.find_element_by_xpath("//div[@id='info']//div[@id='info-contents']//ytd-video-primary-info-renderer//div[@id='container']//div[@id='info']//div[@id='menu-container']//ytd-sentiment-bar-renderer//paper-tooltip//div[@id='tooltip']")
+        likes_and_deslikes_in_video = get_likes_and_dislikes.get_attribute('innerText').split()
+        likes_in_video = likes_and_deslikes_in_video[0]
+        deslikes_in_video = likes_and_deslikes_in_video[-1]
+        likes.append(likes_in_video)
+        print(emoji.emojize(":check_mark: Likes"))
+        deslikes.append(deslikes_in_video)
+        print(emoji.emojize(":check_mark: Deslikes"))
 
-    
-    controller = 10
-    spinner_comments = yaspin(Spinners.earth, text="getting Comments")
-
-
-    with yaspin(Spinners.earth, text="Getting Comments") as sp:
-        while True:
-            browser.execute_script("window.scrollTo(0, {});".format(controller))
-            sleep(0.3)
-            try:
-                get_amount_of_comments = browser.find_element_by_xpath("/html/body/ytd-app/div/ytd-page-manager/ytd-watch-flexy/div[4]/div[1]/div/ytd-comments/ytd-item-section-renderer/div[1]/ytd-comments-header-renderer/div[1]/h2/yt-formatted-string")
-                amount_of_comments = get_amount_of_comments.get_attribute('innerText').split()[0]
-                sp.text = 'Comments amount'
-                sp.ok("✔")
-                break
-            except Exception as error:
-                pass
-            controller += 10
         
-    
-    table.add_row([title, visualization, date, likes_in_video, deslikes_in_video, amount_of_comments])
+        controller = 10
+        spinner_comments = yaspin(Spinners.earth, text="getting Comments")
 
-finish = "Output"
-finish = finish.center(100, '-')
-print(finish)
-print(table)
-    
 
-browser.quit()
+        with yaspin(Spinners.earth, text="Getting Comments") as sp:
+            while True:
+                browser.execute_script("window.scrollTo(0, {});".format(controller))
+                sleep(0.3)
+                try:
+                    get_amount_of_comments = browser.find_element_by_xpath("/html/body/ytd-app/div/ytd-page-manager/ytd-watch-flexy/div[4]/div[1]/div/ytd-comments/ytd-item-section-renderer/div[1]/ytd-comments-header-renderer/div[1]/h2/yt-formatted-string")
+                    amount_of_comments = get_amount_of_comments.get_attribute('innerText').split()[0]
+                    sp.text = 'Comments amount'
+                    sp.ok("✔")
+                    break
+                except Exception as error:
+                    pass
+                controller += 10
+        
+            
+        
+        table.add_row([title, visualization, date, likes_in_video, deslikes_in_video, amount_of_comments, link])
+
+    finish = "Output"
+    finish = finish.center(100, '-')
+    print(finish)
+    print(table)
+        
+
+    browser.quit()
+
+
+if __name__ == '__main__':
+    asyncio.run(main())
